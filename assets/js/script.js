aecac5595d3ec5de4a6c71cfa471edb71429d4ac
Johnes Sombra <jhonesk5@gmail.com>
2025-06-20 21:57:13 -0500

    Modelo Página de Login HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@ function validarLogin() {
   const pass = document.getElementById('password').value.trim();
   const msg = document.getElementById('mensagem');
 
-  if (user === "admin" && pass === "minhasenha123") {
+  if (user === "admin" && pass === "senha123") {
     msg.textContent = "Login bem-sucedido!";
     msg.className = "message success";
+
+    // Redireciona após 1 segundo para a tela principal
+    setTimeout(() => {
+      window.location.href = "../homepage/index.html";
+    }, 1000);
+
   } else {
     msg.textContent = "Usuário ou senha incorretos.";
     msg.className = "message error";
